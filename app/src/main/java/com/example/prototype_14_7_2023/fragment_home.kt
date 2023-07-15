9a57176ab09b887982355cef08f101db5f17c609
Dale Scicluna <dale.scicluna.20@um.edu.mt>
2023-07-15 23:39:34 +0200

    added mapview design

--- app/src/main/java/com/example/prototype_14_7_2023/fragment_home.kt
+++ app/src/main/java/com/example/prototype_14_7_2023/fragment_home.kt
@@ -1,10 +1,13 @@
 package com.example.prototype_14_7_2023
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.CheckBox
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,12 +32,69 @@ class fragment_home : Fragment() {
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+
+       val v = inflater.inflate(R.layout.fragment_home, container, false)
+
+        val check_blue = view?.findViewById <CheckBox> (R.id.blue)
+
+        check_blue?.setOnClickListener{
+
+            if (check_blue.isChecked){
+
+                //checked
+
+            }else{
+
+
+
+            }
+
+        }
+
+
+        val check_red = view?.findViewById<CheckBox>(R.id.red)
+
+
+        check_red?.setOnClickListener{
+
+            if (check_red.isChecked){
+
+                //checked
+
+            }else{
+
+
+
+            }
+
+        }
+
+
+
+
+
+        val check_green = view?.findViewById<CheckBox>(R.id.green)
+
+
+        check_green?.setOnClickListener{
+
+            if (check_green.isChecked){
+
+                //checked
+
+            }else{
+
+
+
+            }
+
+        }
+
+
+
+        return v
     }
 
     companion object {
